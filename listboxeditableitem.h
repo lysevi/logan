35ba5eed4b892204611f202af3b0090a2d6a94d6
Sergey Lyubimov <lysevi@gmail.com>
2017-07-18 16:01:10 +0300

    display html

--- listboxeditableitem.h
+++ listboxeditableitem.h
@@ -4,11 +4,35 @@
 #include <QItemDelegate>
 #include <QObject>
 #include <QTextEdit>
+#include <QStyleOption>
+#include <QPainter>
 
 class ListboxEditableItem : public QItemDelegate
 {
 public:
     explicit  ListboxEditableItem(QObject  *parent=0);
+
+    void paint(QPainter *painter,
+               const QStyleOptionViewItem &option,
+               const QModelIndex &index) const override{
+
+
+        painter->save();
+
+        auto value = index.model()->data(index, Qt::DisplayRole).toString();
+        QTextDocument doc;
+        doc.setHtml(value);
+
+
+        option.widget->style()->drawControl(QStyle::CE_ItemViewItem, &option, painter);
+
+        painter->translate(option.rect.left(), option.rect.top());
+        QRect clip(0, 0, option.rect.width(), option.rect.height());
+        doc.drawContents(painter, clip);
+
+        painter->restore();
+    }
+
     QWidget *createEditor(QWidget *parent, const QStyleOptionViewItem &option,
                           const QModelIndex &index) const override{
         Q_UNUSED(index);
@@ -17,6 +41,7 @@ public:
         //result->setText(index.data());
         return result;
     }
+
 
     void setEditorData(QWidget *editor, const QModelIndex &index) const override{
         auto value = index.model()->data(index, Qt::EditRole).toString();
