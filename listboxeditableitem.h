3877f67e733c0f29785a133752d8bfb610671181
Sergey Lyubimov <lysevi@gmail.com>
2017-07-22 08:58:24 +0300

    use pixmap to draw lines.

--- listboxeditableitem.h
+++ listboxeditableitem.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <QStyledItemDelegate>
+#include <QStylePainter>
 #include <QObject>
 #include <QTextEdit>
 #include <QStyleOption>
@@ -8,52 +9,14 @@
 #include <QDebug>
 #include "log.h"
 
-class ListboxEditableItem : public QStyledItemDelegate
+class ListboxEditableItem : public QAbstractItemDelegate
 {
 public:
     explicit  ListboxEditableItem(QObject  *parent=0);
 
     void paint(QPainter *painter,
                const QStyleOptionViewItem &option,
-               const QModelIndex &index) const override{
-
-
-        painter->save();
-
-        //painter->setFont(m_default_font);
-
-        auto value = index.model()->data(index, Qt::DisplayRole).toString();
-        QTextDocument doc;
-        QPalette::ColorGroup cg = option.state & QStyle::State_Enabled
-                                  ? QPalette::Normal : QPalette::Disabled;
-        if (cg == QPalette::Normal && !(option.state & QStyle::State_Active)){
-            cg = QPalette::Inactive;
-        }
-        QColor textColor = option.palette.color(cg, QPalette::Text);
-        doc.setDefaultStyleSheet(QString("body { color: %1}").arg(textColor.name()));
-        doc.setDefaultFont(option.font);
-
-
-        // This is the actual code for word wrap.
-        QTextOption txtOption;
-        txtOption.setWrapMode(QTextOption::WrapAtWordBoundaryOrAnywhere);
-        doc.setDefaultTextOption(txtOption);
-        //doc.setTextWidth(rect.width());
-        doc.setHtml(value);
-        doc.setDocumentMargin(1);
-        doc.setDefaultFont(m_default_font);
-
-        //qDebug()<<"font:"<<m_default_font.toString()<<" => "<<doc.defaultFont().toString();
-        option.widget->style()->drawControl(QStyle::CE_ItemViewItem, &option, painter);
-
-        painter->translate(option.rect.left(), option.rect.top());
-
-        QRect clip(0, 0, option.rect.width(), option.rect.height());
-
-        doc.drawContents(painter, clip);
-
-        painter->restore();
-    }
+               const QModelIndex &index) const override;
 
     QSize sizeHint(const QStyleOptionViewItem &option,
                    const QModelIndex &index) const override{
